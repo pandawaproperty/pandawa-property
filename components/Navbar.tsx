c9844cc26ff48cf11a0991ed880fe0831e3cfdf6
Mirsah Hadi <mirsamidun@192.168.1.9>
2026-09-01 16:02:50 +0700

    Update navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -37,7 +37,7 @@ export default function Navbar() {
 
           <Link href="/" onClick={closeMenu}>
             <Image
-  src="/images/logo/pandawa.png"
+  src="/images/logo/pandawa2.png"
   alt="Pandawa Property"
   width={520}
   height={150}
